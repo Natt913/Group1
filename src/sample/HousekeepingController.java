81fc608c8af151dad3d66218c23a036e216b7134
Natt913 <nathalie.crespo913@gmail.com>
2018-10-21 10:55:35 -0400

    Housekeeping requests screen.

---
+++ src/sample/HousekeepingController.java
@@ -0,0 +1,23 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
+import javafx.scene.text.Text;
+
+import javax.swing.*;
+
+public class HousekeepingController {
+    @FXML
+    private MenuButton menuButton;
+
+    @FXML
+    MenuItem homeButton;
+
+    @FXML
+    MenuItem profileButton;
+
+
+}
