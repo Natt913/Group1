c188376f624a0e04d030800c7c9400df6beca4e6
Natt913 <nathalie.crespo913@gmail.com>
2018-10-22 10:16:57 -0400

    Housekeeping requests screen.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("Housekeeping.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("HotelGUI");
         primaryStage.setScene(new Scene(root, 640, 480));
         primaryStage.show();
     }
